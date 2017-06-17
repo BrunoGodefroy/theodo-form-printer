56ee20960bbc8b0063ba834c162e838b00d949cc
BrunoGodefroy <brunog@theodo.co.uk>
2017-06-17 20:57:23 +0100

    Login on click of login button

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,26 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 
 import loader from './images/loader.gif'
+import { loginRequest } from './redux/actions';
 
 
 class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(event) {
+    event.preventDefault()
+    this.props.loginRequest()
+  }
+
   render() {
     return <div>
       <h1>Theodo Project Form - Print Me</h1>
-      <button>{ this.props.loggedIn ? 'Logout' : 'Login' }</button>
+      { this.props.loggedIn ? <button onClick={this.handleLogin}>Logout</button> :
+        <button>Login</button>
+      }
       { this.props.loading && <div><img src={loader} width="100"/></div> }
     </div>;
   }
@@ -17,6 +30,7 @@ class App extends PureComponent {
 App.propTypes = {
   loggedIn: React.PropTypes.bool.isRequired,
   loading: React.PropTypes.bool.isRequired,
+  loginRequest: React.PropTypes.func.isRequired,
 }
 
 const mapStateToProps = ({ loggedIn, loading }) => ({
@@ -24,4 +38,9 @@ const mapStateToProps = ({ loggedIn, loading }) => ({
   loading,
 });
 
-export default connect(mapStateToProps)(App)
+const mapDispatchToProps = {
+  loginRequest,
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
