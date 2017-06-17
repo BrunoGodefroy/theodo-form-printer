2da56536a8d3841205ff6578e0c2a1088a2b0f50
BrunoGodefroy <brunog@theodo.co.uk>
2017-06-17 17:02:51 +0100

    Add webpack config and build react

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <div>Hello World!</div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.jsx'],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|png|svg)$/,
+        use: ['url-loader', 'img-loader']
+      },
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+        exclude: /\landing.less$/
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      minimize: true,
+      comments: false,
+      sourceMap: true,
+    }),
+  ]
+};
