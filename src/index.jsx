ae8a3b6fe68aeaa206e284f046ec0a8a9747b9f4
BrunoGodefroy <brunog@theodo.co.uk>
2017-06-17 17:30:12 +0100

    Setup redux store and reducer

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import store from './redux/store';
+
 ReactDOM.render(
-  <div>Hello World!</div>,
+  <Provider store={store}>
+    <div>Hello World!</div>
+  </Provider>,
   document.getElementById('app')
 );
