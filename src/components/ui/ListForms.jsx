dd8579026bbae1a2579afb2e170eacc399275002
BrunoGodefroy <brunog@theodo.co.uk>
2017-06-19 08:36:17 +0100

    Use sematic UI List element

--- src/components/ui/ListForms.jsx
+++ src/components/ui/ListForms.jsx
@@ -1,15 +1,21 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { List, Segment } from 'semantic-ui-react';
 
 class ListForms extends PureComponent {
   render() {
-    return <ul>
-      { Object.keys(this.props.forms).map(project =>
-        <li key={ `key-${project}` }>
-          <a target="_blank" href={ this.props.forms[project] }>{ project }</a>
-        </li>
-      ) }
-    </ul>;
+    return <Segment textAlign="left">
+      <List relaxed divided>
+        { Object.keys(this.props.forms).map(project =>
+          <List.Item key={ `key-${project}` } target="_blank" href={ this.props.forms[project] }>
+            <List.Icon name="file text outline" />
+            <List.Content>
+              { project }
+            </List.Content>
+          </List.Item>
+        ) }
+      </List>
+    </Segment>;
   }
 }
 
