d2aba3fb07271fcd43fde0fc218a4548378df8ec
Sammy Teillet <sammy.teillet@gmail.com>
2017-06-30 15:15:46 +0100

    French printer

--- src/services/google.js
+++ src/services/google.js
@@ -1,9 +1,10 @@
 const gapi = window.gapi
 
-export const CLIENT_ID = '932358196218-67c89d4v3v5tif0hf4ta2uvu3n365db8.apps.googleusercontent.com';
+export const CLIENT_ID = '14597778711-d4kt5ktli45oohinerhi2u3l4rolbaoe.apps.googleusercontent.com	';
 export const DISCOVERY_DOCS = ["https://script.googleapis.com/$discovery/rest?version=v1"];
 export const SCOPES = 'https://www.googleapis.com/auth/forms';
-export const SCRIPT_ID = 'MeRye5A_wHzCuyYvIJ85WNJ6eS0vCcuvm';
+export const SCRIPT_ID_UK = 'MeRye5A_wHzCuyYvIJ85WNJ6eS0vCcuvm';
+export const SCRIPT_ID = 'MopOUf2aDvG7Y1O3Ltt_E_RT6drthxiAf';
 
 gapi.loadAsync = () => new Promise(
   (resolve, reject) => {
