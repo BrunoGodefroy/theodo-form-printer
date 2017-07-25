68baba91a70f85f732f20e08729964022425d784
Sammy Teillet <sammy.teillet@gmail.com>
2017-07-25 17:42:46 +0100

    Fix display of satisfaction overview

--- src/components/ui/ListForms.jsx
+++ src/components/ui/ListForms.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Accordion, Label } from 'semantic-ui-react';
+import { Accordion, Label, Container } from 'semantic-ui-react';
 import ProjectForm from './ProjectForm';
 import { companies } from '../../redux/actions';
 
@@ -35,7 +35,18 @@ class ListForms extends PureComponent {
     });
 
     if (this.props.forms.length > 0) {
-      return <Accordion panels={ panels } styled />
+      return (
+        <Container style={{ margin: '10px', display: 'flex', flexDirection: 'column' }}>
+          <Container textAlign="center">
+            <Label color="green" >WOW: {this.props.numberOfWahou} / {this.props.forms.length} - {Math.round(this.props.numberOfWahou*100/this.props.forms.length)}%</Label>
+            <Label color="olive" >Success: {this.props.numberOfOK} / {this.props.forms.length} - {Math.round(this.props.numberOfOK*100/this.props.forms.length)}%</Label>
+            <Label color="red" >Red Bucket: {this.props.numberOfKO} / {this.props.forms.length} - {Math.round(this.props.numberOfKO*100/this.props.forms.length)}%</Label>
+          </Container>
+          <Container style={{ marginTop: '10px', display: 'flex', justifyContent: 'center' }} textAlign="left">
+            <Accordion panels={ panels } styled />
+          </Container>
+        </Container>
+      )
     } else {
       return <p>To load the latest form, please click on "update the latest forms"</p>;
     }
@@ -46,6 +57,9 @@ ListForms.propTypes = {
   forms: PropTypes.array.isRequired,
   questions: PropTypes.array.isRequired,
   company: PropTypes.string.isRequired,
+  numberOfWahou: PropTypes.number.isRequired,
+  numberOfOK: PropTypes.number.isRequired,
+  numberOfKO: PropTypes.number.isRequired,
 }
 
 export default ListForms;
