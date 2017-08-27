5b6d04300d1a8ae85153fc9dd785fb7376672924
BrunoGodefroy <brunog@theodo.co.uk>
2017-08-27 10:01:12 +0100

    Refactor the app into several logic component

---
+++ src/components/logic/ListForms.jsx
@@ -0,0 +1,82 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { Accordion, Label, Container } from 'semantic-ui-react'
+import { ProjectForm } from '../ui'
+
+class ListForms extends PureComponent {
+  render () {
+    const panels = this.props.forms.map((form, index) => {
+      let label
+      switch (form.satisfaction) {
+        case 'Waouh':
+          label = <Label color='green' className='pinned'>WOW!</Label>
+          break
+        case 'OK':
+          label = <Label color='olive' className='pinned'>Success</Label>
+          break
+        case 'KO':
+          label = <Label color='red' className='pinned'>Red bucket</Label>
+          break
+        default:
+          label = <Label color='pink' className='pinned'>error</Label>
+      }
+
+      const formTitle = form.sprint ? `${form.project} - Sprint ${form.sprint}` : `${form.project}`
+      return {
+        title: <span>{formTitle} {label}</span>,
+        content: <ProjectForm form={form} questions={this.props.questions} company={this.props.selectedCompany.name} />,
+        key: `${form.project} - Sprint ${form.sprint} - ${index}`
+      }
+    })
+
+    if (this.props.forms.length > 0) {
+      return <Container className={`company-${this.props.selectedCompany.name}`} >
+        <Container style={{ margin: '10px', display: 'flex', flexDirection: 'column' }} className='no-print'>
+          <Container textAlign='center'>
+            <Label color='green' >WOW: {this.props.numberOfWahou} / {this.props.forms.length} - {Math.round(this.props.numberOfWahou * 100 / this.props.forms.length)}%</Label>
+            <Label color='olive' >Success: {this.props.numberOfOK} / {this.props.forms.length} - {Math.round(this.props.numberOfOK * 100 / this.props.forms.length)}%</Label>
+            <Label color='red' >Red Bucket: {this.props.numberOfKO} / {this.props.forms.length} - {Math.round(this.props.numberOfKO * 100 / this.props.forms.length)}%</Label>
+          </Container>
+        </Container>
+        <Container style={{ display: 'flex', justifyContent: 'center' }} textAlign='left'>
+          <Accordion panels={panels} styled />
+        </Container>
+      </Container>
+    } else {
+      return <Container as='p'>To load the latest form, please click on "update the latest forms"</Container>
+    }
+  }
+}
+
+ListForms.propTypes = {
+  forms: PropTypes.array.isRequired,
+  questions: PropTypes.array.isRequired,
+  selectedCompany: PropTypes.object.isRequired,
+  numberOfWahou: PropTypes.number.isRequired,
+  numberOfOK: PropTypes.number.isRequired,
+  numberOfKO: PropTypes.number.isRequired,
+  loading: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = ({
+  loading,
+  forms,
+  questions,
+  selectedCompany,
+  numberOfWahou,
+  numberOfOK,
+  numberOfKO
+}) => ({
+  loading,
+  forms,
+  questions,
+  selectedCompany,
+  numberOfWahou,
+  numberOfOK,
+  numberOfKO
+})
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListForms)
---
+++ src/components/logic/SelectCompanyButtons.jsx
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { Button, Container } from 'semantic-ui-react'
+import { chooseCompany, companies } from '../../redux/actions'
+
+class SelectCompanyButtons extends PureComponent {
+  constructor (props) {
+    super(props)
+    this.selectCompany = this.selectCompany.bind(this)
+  }
+
+  selectCompany (company) {
+    return () => this.props.chooseCompany(company)
+  }
+
+  render () {
+    return <Container>
+      <Button active={this.props.selectCompany === companies.THEODO_UK} onClick={this.selectCompany(companies.THEODO_UK)} >Theodo UK</Button>
+      <Button active={this.props.selectCompany === companies.THEODO_FR} onClick={this.selectCompany(companies.THEODO_FR)} >Theodo FR</Button>
+      <Button active={this.props.selectCompany === companies.FASTIT} onClick={this.selectCompany(companies.FASTIT)} >FastIT</Button>
+      <Button active={this.props.selectCompany === companies.BAM} onClick={this.selectCompany(companies.BAM)} >BAM</Button>
+      <Button active={this.props.selectCompany === companies.SICARA} onClick={this.selectCompany(companies.SICARA)} >Sicara</Button>
+    </Container>
+  }
+}
+
+SelectCompanyButtons.propTypes = {
+  chooseCompany: PropTypes.func.isRequired,
+  selectedCompany: PropTypes.object
+}
+
+const mapStateToProps = ({ selectedCompany }) => ({
+  selectedCompany
+})
+
+const mapDispatchToProps = {
+  chooseCompany
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SelectCompanyButtons)
