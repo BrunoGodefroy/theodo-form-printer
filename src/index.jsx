9b1bb645877fddaf39842907cc05beb495bb9ab5
BrunoGodefroy <brunog@theodo.co.uk>
2017-10-23 09:19:51 +0100

    Replace existing ga script by react ga

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import ReactGA from 'react-ga'
 
 import store from '@redux/store'
 import App from '@components/App'
+
+ReactGA.initialize('UA-102505380-1', { debug: true })
+ReactGA.pageview('/')
 
 ReactDOM.render(
   <Provider store={store}>
