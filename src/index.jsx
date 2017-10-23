f34c266c5f1772d4036e2c3cdb8243932bcf7b8e
BrunoGodefroy <brunog@theodo.co.uk>
2017-10-23 09:25:24 +0100

    Remove debug

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import ReactGA from 'react-ga'
 import store from '@redux/store'
 import App from '@components/App'
 
-ReactGA.initialize('UA-102505380-1', { debug: true })
+ReactGA.initialize('UA-102505380-1')
 ReactGA.pageview('/')
 
 ReactDOM.render(
